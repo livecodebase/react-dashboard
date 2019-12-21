145ffe55f10d7496c5971d54c7f3cc39aa6477ae
livecodebase <lunarshadeofficial@outlook.com>
2019-12-21 19:20:42 +0530

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")]
+  plugins: [tailwindcss("./src/tailwind.js"), require("autoprefixer")]
 };
